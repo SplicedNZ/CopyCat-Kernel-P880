63da7dce2aa092564b3dac1527839d4a6ebd4a3f
laufersteppenwolf <laufersteppenwolf@gmail.com>
2013-05-09 16:16:56 +0200

    fixed conflicts
    Conflicts:
    	arch/arm/mach-tegra/cpu-tegra.c
    	arch/arm/mach-tegra/edp.c
    	arch/arm/mach-tegra/tegra3_clocks.c
    	arch/arm/mach-tegra/tegra3_dvfs.c
    	drivers/cpufreq/cpufreq.c
    	drivers/video/tegra/host/nvhost_acm.c

--- drivers/video/tegra/host/nvhost_acm.c
+++ drivers/video/tegra/host/nvhost_acm.c
@@ -149,6 +149,8 @@ static void to_state_running_locked(struct nvhost_device *dev)
 				return;
 			}
 		}
+		for (i = 0; i < mod->num_clks; i++) 
+			clk_enable(mod->clk[i]);
 
 		/* Invoke callback after enabling clock. This is used for
 		 * re-enabling host1x interrupts. */
